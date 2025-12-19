4056138ffb63b7d10f07b39344e4560320f57318
Ogochukwu Nwachukwu <ogochukwu.nwachukwu@m-kopa.com>
2025-12-19 12:17:08 +0100

    first commit

---
+++ posts/views.py
@@ -0,0 +1,12 @@
+from django.views.generic import ListView
+from .models import Post
+
+# Create your views here.
+class HomePageView(ListView):
+    model = Post
+    template_name = 'home.html'
+    context_object_name = 'all_posts_list'
+
+    # def get_queryset(self):
+    #     return Post.objects.all()
+
